a6de624bc51ff5eb6be9b167ee8177754d59ac3e
nrkhmd <157031226+nrkhmd@users.noreply.github.com>
2026-02-23 16:00:51 +0700

    Create bot_emas.py

---
+++ bot_emas.py
@@ -0,0 +1,9 @@
+import os
+import pandas as pd
+# ... (import lainnya sama)
+
+# Ambil dari GitHub Secrets (Lebih aman!)
+TOKEN = os.environ.get('8544691510:AAFZPm6QsANs6sGFhIrYpcuRvkdWcQgb3wc')
+CHAT_ID = os.environ.get('8544691510')
+
+# ... (Logika calculate_indicators, SL/TP, dan analyze sama seperti sebelumnya)
